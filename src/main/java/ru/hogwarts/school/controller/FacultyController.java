b4db548cf7c8e726a783c754365c96fc1777fcc7
Ani <ani-alaverdyan@mail.ru>
2022-12-04 18:32:08 +0300

    commit

---
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -0,0 +1,61 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.service.FacultyService;
+import java.util.List;
+
+@RestController
+@RequestMapping("faculty")
+public class FacultyController {
+    private final FacultyService facultyService;
+
+    public FacultyController(FacultyService facultyService) {
+        this.facultyService = facultyService;
+    }
+
+    @PostMapping // POST http:localhost:8080/faculty
+    public ResponseEntity<Faculty> createFaculty(@RequestBody Faculty faculty) {
+        Faculty createdFaculty = facultyService.createFaculty(faculty);
+        return ResponseEntity.ok(createdFaculty);
+    }
+
+    @GetMapping("{id}") // GET http://localhost:8080/faculty/1
+    public ResponseEntity<Faculty> readFaculty(@PathVariable Long id) {
+        Faculty faculty = facultyService.readFaculty(id);
+        if (faculty == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(faculty);
+    }
+
+    @PutMapping // PUT http"//localhost:8080/faculty
+    public ResponseEntity<Faculty> updateFaculty(@RequestBody Faculty faculty) {
+        Faculty updatedFaculty = facultyService.updateFaculty(faculty);
+        if (updatedFaculty == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(updatedFaculty);
+    }
+
+    @DeleteMapping("{id}") // DELETE http"//localhost:8080/faculty/1
+    public ResponseEntity deleteFaculty(@PathVariable Long id) {
+        facultyService.deleteFaculty(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping // GET http"//localhost:8080/faculty
+    public ResponseEntity<List<Faculty>> getFaculties() {
+        return ResponseEntity.ok(facultyService.getFaculties());
+    }
+
+    //@GetMapping("/colour") // GET http"//localhost:8080/faculty?colour=
+    //public ResponseEntity<List<Faculty>> getFacultiesByColour(@RequestParam String colour){
+    //    if (colour==null && colour.isBlank()){
+    //       return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    //    }
+    //  return ResponseEntity.ok(facultyService.getFacultiesByColour(colour));
+    // }
+}
---
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -0,0 +1,64 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.service.StudentService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("student")
+public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping //POST http://localhost:8080/students/2
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+        Student createdStudent = studentService.createStudent(student);
+        return ResponseEntity.ok(createdStudent);
+    }
+
+    @GetMapping("{id}") //GET http://localhost:8080/students/2
+    public ResponseEntity<Student> readStudent(@PathVariable Long id) {
+        Student student = studentService.readStudent(id);
+        if (student == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(student);
+    }
+
+    @PutMapping //PUT http://localhost:8080/students/2
+    public ResponseEntity<Student> updateStudent(@RequestBody Student student) {
+        Student updatedStudent = studentService.updateStudent(student);
+        if (updatedStudent == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(updatedStudent);
+
+    }
+
+    @DeleteMapping("{id}") //Delete http://localhost:8080/students/2
+    public ResponseEntity deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping //GET http://localhost:8080/students/2
+    public ResponseEntity<List<Student>> getStudents() {
+        return ResponseEntity.ok(studentService.getStudents());
+    }
+
+    //@GetMapping("/age") //GET http://localhost:8080/students/2
+    // public ResponseEntity<List<Student>> getStudentsByAge(@RequestParam Integer age){
+    //    if (age<7||age>60){
+    //        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    //   }
+    //  return ResponseEntity.ok(studentService.getStudentsByAge(age));
+    //}
+}
+
