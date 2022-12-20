1e44b66fe933ae86146ef290c70b7ede5a26893e
Ani <ani-alaverdyan@mail.ru>
2022-12-20 21:29:58 +0300

    commit

--- src/main/java/ru/hogwarts/school/scripts_422.sql
+++ src/main/java/ru/hogwarts/school/scripts_422.sql
@@ -1,2 +1,11 @@
-CREATE TABLE cars (id SERIAL PRIMARY KEY ,brand VARCHAR, model VARCHAR, price NUMERIC CHECK ( price>0));
-CREATE TABLE drivers (id SERIAL PRIMARY KEY, name VARCHAR,age INTEGER,license BOOLEAN,car_id INTEGER REFERENCES car(id));
+CREATE TABLE cars (
+    id SERIAL PRIMARY KEY,
+    brand VARCHAR,
+    model VARCHAR,
+    price NUMERIC CHECK ( price>0));
+CREATE TABLE drivers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR,
+    age INTEGER,
+    license BOOLEAN,
+    car_id INTEGER REFERENCES car(id));
